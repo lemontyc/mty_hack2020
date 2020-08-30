d2645b0e0f1d5181c32e0613c27532238b8af7b6
Luis Montoya <a01208334@itesm.mx>
2020-08-29 21:14:29 -0500

    [FEAT] Implemented Auth0 authentication
    API is able to obtain name and photo byb registering by mail. need to test facebook other logins.

--- main.js
+++ main.js
@@ -1,43 +1,26 @@
-const { app, BrowserWindow } = require('electron')
+// main.js
 
-function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+const {app} = require('electron');
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+const {createAuthWindow} = require('./main/auth-process');
+const createAppWindow = require('./main/app-process');
+const authService = require('./services/auth-service');
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+async function showWindow() {
+  try {
+    await authService.refreshTokens();
+    return createAppWindow();
+  } catch (err) {
+    createAuthWindow();
+  }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.on('ready', showWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  app.quit();
+});
