573a5aa25024c132e39d9a8603452cd182ffacc3
Ivan Ochoa <ivan.aochoa.g@gmail.com>
2020-08-30 00:40:10 -0500

    L'ogica para el 25% max listo
    La matriz de 10x10 se llnea del 1 al 25 con susana distancia  :)

--- cppFiles/test.cpp
+++ cppFiles/test.cpp
@@ -2,30 +2,87 @@
 #include <deque>
 #include <algorithm>
 #include <utility>
+#include <stack>
 
 int main(){
     
-    int n,m; //rows,colums
-    int counter = 0
-    std::deque<int> myseats;
+    int n,m,p; //rows,colums
+    int counter = 1;
+    std::stack<std::pair<int,int>> myseats;
     int seats[10][10];
-    std::pair <int,int> start( (n/2), (m/2) );
+
+    std::cin >> n >> m >> p;
+    n--;
+    m--;
+
+    std::pair <int,int> center;
+    std::pair <int,int> currentSeat;
     
     for (char i = 0; i < 10; i++)
     {
-        std::fill(seats[i][0],seats[0][10],0);
+        for (char j = 0; j < 10; j++)
+        {
+            seats[i][j] = 0;
+        }
+        
+    }
+
+    myseats.push(std::pair<int,int>((n/2),(n/2)));
+
+    seats[n/2][n/2] = counter++;
+    p--;
+     
+    while ((!myseats.empty()) && (p > 0))
+    {   
+        center = myseats.top();
+        myseats.pop();
+
+        // Left seat
+        if( center.first + 2 <= n && seats[center.first + 2][center.second] == 0 && p > 0){
+            seats[center.first + 2][center.second] = counter++;
+            // Saving actual seat
+            currentSeat.first = center.first + 2;
+            currentSeat.second = center.second;
+            myseats.push(currentSeat);
+            p--;
+        }
+
+        // up seat
+        if (center.second + 2 <= m && seats[center.first][center.second + 2] == 0 && p > 0){
+            seats[center.first][center.second + 2] = counter++;
+            currentSeat.first = center.first;
+            currentSeat.second = center.second + 2;
+            myseats.push(currentSeat);
+            p--;
+        }
+        //rigth seat
+        if (center.first - 2 >= 0 && seats[center.first - 2][center.second] == 0 && p > 0){
+            seats[center.first - 2][center.second] = counter++;
+            currentSeat.first = center.first - 2;
+            currentSeat.second = center.second;
+            myseats.push(currentSeat);
+            p--;
+        }
+        //bottom seat
+        if (center.second - 2 >= 0 && seats[center.first][center.second - 2] == 0 && p > 0){
+            seats[center.first][center.second - 2] = counter++;
+            currentSeat.first = center.first;
+            currentSeat.second = center.second - 2;
+            myseats.push(currentSeat); 
+            p--;
+        }
+    } 
+
+    for (char i = 0; i < 10; i++)
+    {
+        for (char j = 0; j < 10; j++)
+        {
+            std::cout << seats[i][j] << " ";
+        }
+        std::cout << std::endl;
     }
     
-    //center
-    seats[start.first][start.second] = ++counter;
-    // Left seat
-    seats[start.first + 2][start.second] = ++counter;
-    // up seat
-    seats[start.first][start.second + 2] = ++counter;
-    //rigth seat
-    seats[start.first - 2][start.second] = ++counter;
-    //bottom seat
-    seats[start.first][start.second - 2] = ++counter;
+
     
 }
 
